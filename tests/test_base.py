b1243a0cd577f0ebfbd5f6c47fc10795a400cb2a
Isaac Luke Smith <Zenohm@users.noreply.github.com>
2018-10-12 22:42:21 -0400

    fix: pep8 violations

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,25 +1,24 @@
 import array
 
-import array
-
 # PPM header
 width = 256
 height = 128
 maxval = 255
-ppm_header = 'P6 {} {} {}\n'.format(width,height,maxval)
+ppm_header = 'P6 {} {} {}\n'.format(width, height, maxval)
 
 # PPM image data (filled with blue)
 image = array.array('B', [0, 0, 0] * width * height)
 
-# Fill with red the rectangle with origin at (10, 10) and width x height = 50 x 80 pixels
+# Create a red rectangle with origin at (10, 10)
+# The width x height = 50 x 80 pixels
 for y in range(10, 90):
-	for x in range(10, 60):
- 		index = 3 * (y * width + x)
-		image[index] = 255           # red channel
-		image[index + 1] = 255         # green channel
- 		image[index + 2] = 255         # blue channel
- 
+    for x in range(10, 60):
+        index = 3 * (y * width + x)
+        image[index] = 255           # red channel
+        image[index + 1] = 255       # green channel
+        image[index + 2] = 255       # blue channel
+
 # Save the PPM image as a binary file
 with open('blue_red_example.ppm', 'wb') as f:
-	f.write(bytearray(ppm_header, 'ascii'))
-	image.tofile(f)
+    f.write(bytearray(ppm_header, 'ascii'))
+    image.tofile(f)
--- tests/test_bw_image.py
+++ tests/test_bw_image.py
@@ -1,14 +1,12 @@
 import sys
+import numpy as np
+import ppmimpy as ppy
+
 sys.path.append("./")
 
-import numpy as np
-import  ppmimpy as ppy
 test = ppy.ppmimpy()
-arr = np.zeros(shape=(32,128))
-arr[4:10,10:100] = 1
-arr[24:30,120:300] = 1
+arr = np.zeros(shape=(32, 128))
+arr[4:10, 10:100] = 1
+arr[24:30, 120:300] = 1
 test.array_to_image(arr)
 test.create_file('temp')
-
-
-
