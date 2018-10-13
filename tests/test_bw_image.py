86f3c127ec4efbe80fb7f0e95603c15d1fe9730d
Isaac Luke Smith <Zenohm@users.noreply.github.com>
2018-10-12 23:28:28 -0400

    fix: allow pytest to run the tests properly

--- tests/test_bw_image.py
+++ tests/test_bw_image.py
@@ -1,9 +1,15 @@
 import numpy as np
 from src import ppmimpy as ppy
 
-test = ppy.ppmimpy()
-arr = np.zeros(shape=(32, 128))
-arr[4:10, 10:100] = 1
-arr[24:30, 120:300] = 1
-test.array_to_image(arr)
-test.create_file('temp')
+
+def create_black_and_white_image():
+    test = ppy.ppmimpy()
+    arr = np.zeros(shape=(32, 128))
+    arr[4:10, 10:100] = 1
+    arr[24:30, 120:300] = 1
+    test.array_to_image(arr)
+    test.create_file('ppms/temp')
+
+
+def test_can_create_black_and_white_image():
+    create_black_and_white_image()
